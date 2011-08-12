6d238de0cb79637b241909e46888c2a2d477f714
Alexander Senko <Alexander.Senko@gmail.com>
2011-08-12 05:14:29 +0400

    Framework: new _Rails 3.1_ application.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Barter::Application.config.secret_token = 'a9401afce8dccd6111e0dacd10175d64e66dd892378f4e8d3dbb771affeb51f371d2856d70a49ee6379f9accc380f9052d0bbb75c2fc5d7def5b5c2d7dda7166'
